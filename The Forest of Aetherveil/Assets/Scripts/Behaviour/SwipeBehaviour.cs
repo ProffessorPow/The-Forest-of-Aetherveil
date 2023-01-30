712567fef429724a2fbbd7a7474bed0329eab8fc
Jarrett Randall <jarrett.randall3@gmail.com>
2023-01-29 22:00:46 -0700

    Finally got touch inputs working
    Implementing logic and triggers tomorrow

---
+++ The Forest of Aetherveil/Assets/Scripts/Behaviour/SwipeBehaviour.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeBehaviour : MonoBehaviour
+{
+    private float startTouchLoc;
+    private Vector2 endTouchLoc;
+    
+    
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            
+        }
+    }
+}
